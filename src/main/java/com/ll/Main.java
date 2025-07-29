660ec591dbb91354f8b54042c998f9d216ca4873
gregoryc103 <gregoryc103@gmail.com>
2025-07-29 17:11:18 +0900

    initial commit

---
+++ src/main/java/com/ll/Main.java
@@ -0,0 +1,13 @@
+package com.ll;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("명언 앱");
+
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
